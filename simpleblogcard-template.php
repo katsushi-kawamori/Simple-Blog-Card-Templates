0257ea08b79512caf963a8b9ce01de0686cc3ba4
katsushi-kawamori <dodesyoswift312@gmail.com>
2024-03-30 23:12:04 +0900

    Update template

--- simpleblogcard-template.php
+++ simpleblogcard-template.php
@@ -5,33 +5,33 @@
  * @package WordPress
  * @subpackage Simple Blog Card
  * @since Simple Blog Card 2.00
- * @version 1.02
+ * @version 1.03
  */
 
 ?>
 
 <style>
 .simpleblogcard_img_block<?php echo esc_html( $hash ); ?> {
-  float: <?php echo esc_attr( $settings['img_pos'] ); ?>;
+  float: <?php echo esc_attr( $img_pos ); ?>;
   padding: 10px;
 }
 .simpleblogcard_border<?php echo esc_html( $hash ); ?> {
-  border-<?php echo esc_attr( $settings['border_pos'] ); ?>: solid <?php echo esc_attr( $settings['color_width'] ); ?>px <?php echo esc_attr( $settings['color'] ); ?>;
+  border-<?php echo esc_attr( $border_pos ); ?>: solid <?php echo esc_attr( $color_width ); ?>px <?php echo esc_attr( $color ); ?>;
   padding: 0.25em 0.25em;
   background: transparent;
 }
 .simpleblogcard_title<?php echo esc_html( $hash ); ?> {
-  line-height: <?php echo esc_attr( $settings['t_line_height'] ); ?>%;
+  line-height: <?php echo esc_attr( $t_line_height ); ?>%;
   font-weight: bold;
   display: block;
 }
 .simpleblogcard_description<?php echo esc_html( $hash ); ?> {
-  line-height: <?php echo esc_attr( $settings['d_line_height'] ); ?>%;
+  line-height: <?php echo esc_attr( $d_line_height ); ?>%;
   color: #333;
 }
 </style>
 <div class="simpleblogcard_wrap">
-	<?php if ( $settings['target_blank'] ) : ?>
+	<?php if ( $target_blank ) : ?>
 		<a style="text-decoration: none;" href=<?php echo esc_url( $url ); ?> target="_blank" rel="noopener">
 	<?php else : ?>
 		<a style="text-decoration: none;" href=<?php echo esc_url( $url ); ?>>
@@ -47,7 +47,7 @@
 			<div class="simpleblogcard_title<?php echo esc_html( $hash ); ?>">
 				<?php echo esc_html( $title ); ?>
 			</div>
-			<?php if ( $settings['dessize'] > 0 ) : ?>
+			<?php if ( $dessize > 0 ) : ?>
 				<div class="simpleblogcard_description<?php echo esc_html( $hash ); ?>">
 					<?php echo esc_html( $description ); ?>
 				</div>
