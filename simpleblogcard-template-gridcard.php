41b446d80a24a8a89fa1b4043e47b70bc265bb41
ishitaka <ishitaka@xakuro.com>
2024-03-31 16:42:48 +0900

    Compliant with plugin version 2.10.

--- simpleblogcard-template-gridcard.php
+++ simpleblogcard-template-gridcard.php
@@ -10,37 +10,36 @@
 /**
  * Grid Card template for Simple Blog Card plugin.
  *
- * Note: Image size ($imgsize) parameter not support.
- *
- * @version 1.0.5
+ * @version 1.1.0
  *
  * @since 1.0.2
- * @since 1.0.3 Tweaked CSS and split common CSS into file.
- * @since 1.0.4 Tweaked CSS.
+ * @since 1.0.3 Tweaked CSS. Split common CSS into file.
  * @since 1.0.5 Tweaked CSS.
+ * @since 1.1.0 Compliant with Simple Blog Card version 2.10. Supported image size.
  */
 ?>
+<?php $image_span = $imgsize <= 100 ? 3 : 4; ?>
 <style>
 .simpleblogcard-template-gridcard-<?php echo esc_html( $hash ); ?> .card {
-	border-<?php echo esc_attr( $settings['border_pos'] ); ?>: solid <?php echo esc_attr( $settings['color_width'] ); ?>px <?php echo esc_attr( $settings['color'] ); ?>;
+	border-<?php echo esc_attr( $border_pos ); ?>: solid <?php echo esc_attr( $color_width ); ?>px <?php echo esc_attr( $color ); ?>;
 }
 .simpleblogcard-template-gridcard-<?php echo esc_html( $hash ); ?> .card-image {
-	grid-column: <?php echo ( 'left' === $settings['img_pos'] ) ? '1 / span 2' : '5 / span 2'; ?>;
-	order: <?php echo ( 'left' === $settings['img_pos'] ) ? '1' : '2'; ?>;
+	grid-column: <?php echo ( 'left' === $img_pos ) ? '1 / span ' . (int) $image_span : (int) ( 12 - $image_span + 1 ) . ' / span ' . (int) $image_span; ?>;
+	order: <?php echo ( 'left' === $img_pos ) ? '1' : '2'; ?>;
 }
 .simpleblogcard-template-gridcard-<?php echo esc_html( $hash ); ?> .card-content {
-	grid-column: <?php echo $img ? ( 'left' === $settings['img_pos'] ? '3 / span 4' : '1 / span 4' ) : '1 / span 6'; ?>;
-	order: <?php echo ( 'left' === $settings['img_pos'] ) ? '2' : '1'; ?>;
+	grid-column: <?php echo $img ? ( 'left' === $img_pos ? (int) ( $image_span + 1 ) . ' / span ' . (int) ( 12 - $image_span ) : '1 / span ' . (int) ( 12 - $image_span ) ) : '1 / span 12'; ?>;
+	order: <?php echo ( 'left' === $img_pos ) ? '2' : '1'; ?>;
 }
 .simpleblogcard-template-gridcard-<?php echo esc_html( $hash ); ?> .card-title {
-	line-height: <?php echo esc_attr( $settings['t_line_height'] ); ?>%;
+	line-height: <?php echo (int) $t_line_height; ?>%;
 }
 .simpleblogcard-template-gridcard-<?php echo esc_html( $hash ); ?> .card-description {
-	line-height: <?php echo esc_attr( $settings['d_line_height'] ); ?>%;
+	line-height: <?php echo (int) $d_line_height; ?>%;
 }
 </style>
 <div class="simpleblogcard-template-gridcard simpleblogcard-template-gridcard-<?php echo esc_html( $hash ); ?>">
-	<?php if ( $settings['target_blank'] ) : ?>
+	<?php if ( $target_blank ) : ?>
 		<a style="text-decoration: none;" href=<?php echo esc_url( $url ); ?> target="_blank" rel="noopener">
 	<?php else : ?>
 		<a style="text-decoration: none;" href=<?php echo esc_url( $url ); ?>>
@@ -54,10 +53,10 @@
 			<div class="card-content">
 				<p class="card-host"><?php echo esc_html( $host ); ?></p>
 				<p class="card-title"><?php echo esc_html( $title ); ?></p>
-				<?php if ( $settings['dessize'] > 0 ) : ?>
-					<div class="card-description">
+				<?php if ( $dessize > 0 ) : ?>
+					<p class="card-description">
 						<?php echo esc_html( $description ); ?>
-					</div>
+					</p>
 				<?php endif; ?>
 			</div>
 		</div>
